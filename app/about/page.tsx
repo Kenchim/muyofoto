fbf767a97e8568610039a2d4d00e4f4c42fb89d6
KENTO NOBUTA <kentonobuta@KENTOnoMacBook-Pro.local>
2026-06-06 23:01:16 +0900

    change about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ export default function About() {
             <FotoCap
             url="../now.jpg"
             alt="Japan"
-            caption="Shooting in Gifu (January 2026)" 
+            caption="Shooting in Nara (June 2026)" 
             />
             <p className="pb-20"></p>
     </div>
